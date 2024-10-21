8d5a12a19fabc2e33fe354589f3397edbb224826
Abdulmazid24 <a.m.abdulmazid24@gmail.com>
2024-10-21 20:03:20 +0600

    some data displaied

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -1,21 +1,40 @@
 import { useEffect } from 'react';
 import { useState } from 'react';
 import Country from '../Country/Country';
-
+import './Countries.css';
 const Countries = () => {
   const [countries, setCountries] = useState([]);
+  const [visitedFlags, setVisitedFlags] = useState([]);
+  console.log(visitedFlags);
   useEffect(() => {
     fetch('https://restcountries.com/v3.1/all')
       .then(response => response.json())
       .then(data => setCountries(data));
   }, []);
+
+  const handleVisitedFlags = flag => {
+    const newVisitedFlags = [...visitedFlags, flag];
+    setVisitedFlags(newVisitedFlags);
+  };
+
   return (
-    <div>
-      <h3>Countries:- {countries.length}</h3>
-      {countries.map(country => (
-        <Country key={country.ccn3} country={country}></Country>
-      ))}
-    </div>
+    <main>
+      <h3>World Countries: {countries.length}</h3>
+      <div className="flag-container">
+        {visitedFlags.map((flag, index) => (
+          <img src={flag} key={index}></img>
+        ))}
+      </div>
+      <div className="country-container">
+        {countries.map(country => (
+          <Country
+            key={country.ccn3}
+            country={country}
+            handleVisitedFlags={handleVisitedFlags}
+          ></Country>
+        ))}
+      </div>
+    </main>
   );
 };
 
--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,14 +1,35 @@
+import { useState } from 'react';
 import './Country.css';
 
-const Country = ({ country }) => {
-  console.log(country);
-  const { name, flags, maps } = country;
+const Country = ({ country, handleVisitedFlags }) => {
+  console.log(handleVisitedFlags);
+  const { name, flags, capital, population, area } = country;
+  const [visited, setVisited] = useState(false);
+  const handleVisited = () => {
+    setVisited(!visited);
+  };
   return (
-    <div className="country">
-      <div>
+    <div className={`country ${visited ? 'visited' : 'no-visited'}`}>
+      <div className="img-container">
         <img src={flags.png} alt="" />
       </div>
-      <h3>{name.common}</h3>
+
+      <div style={{ color: visited ? 'green' : 'black' }}>
+        <h3>{name.common}.</h3>
+        <h4>Capital: {capital}.</h4>
+
+        <p>Population: {population}</p>
+        <p>Area: {area}</p>
+      </div>
+      <button onClick={handleVisited}>{visited ? 'Visited' : 'Going'}</button>
+      {visited ? 'I have Visited this Country.' : 'I want to visited'}
+      <button
+        onClick={() => {
+          handleVisitedFlags(country.flags.png);
+        }}
+      >
+        Add Flag
+      </button>
     </div>
   );
 };
